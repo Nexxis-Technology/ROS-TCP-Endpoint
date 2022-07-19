45be14758d5e111ba4f05b20d6ca219c517d1f7e
reece-the-robot <reece.holland@integratedio.com.au>
2022-07-19 11:20:48 +0800

    NC-88 refactor: changed default node name

--- ros_tcp_endpoint/default_server_endpoint.py
+++ ros_tcp_endpoint/default_server_endpoint.py
@@ -7,7 +7,7 @@ from ros_tcp_endpoint import TcpServer
 
 def main(args=None):
     rclpy.init(args=args)
-    tcp_server = TcpServer("UnityEndpoint")
+    tcp_server = TcpServer("unity_endpoint_node")
 
     tcp_server.start()
 
