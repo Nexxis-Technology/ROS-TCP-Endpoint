fe3d70c9b890b17a3ca4675d7a52810368011fbe
Jihirshu Narayan <jihirshu.narayan@gmail.com>
2026-02-12 16:50:48 +0800

    fix: remove redundant line

--- ros_tcp_endpoint/subscriber.py
+++ ros_tcp_endpoint/subscriber.py
@@ -47,7 +47,6 @@ class RosSubscriber(RosReceiver):
         self.subscription = self.create_subscription(
             self.msg, self.topic, self.send, qos_profile  # queue_size
         )
-        self.subscription
 
     def send(self, data):
         """
